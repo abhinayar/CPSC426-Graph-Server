49a54ecec8cb367567a62126e834ab200680404a
Abhi Nayar <abhi_nayar@Abhis-MacBook-Air.local>
2016-09-22 01:29:12 -0400

    dirty hack

--- server.c
+++ server.c
@@ -207,7 +207,7 @@ int main(int argc, char *argv[])
 
 	int create_socket, new_socket;    
 	socklen_t addrlen;    
-	int bufsize = 1024;    
+	int bufsize = 5024;    
 	char *buffer = malloc(bufsize);    
 	struct sockaddr_in address;    
 
@@ -225,7 +225,8 @@ int main(int argc, char *argv[])
 
 	struct Graph* graph = createGraph();
 
-	while (keepGoing) {    
+	while (keepGoing) {
+        bzero(buffer, bufsize);
         if (listen(create_socket, 10) < 0) {    
          perror("server: listen");    
          exit(1);    
@@ -241,7 +242,7 @@ int main(int argc, char *argv[])
         }
 
         recv(new_socket, buffer, bufsize, 0);    
-        //printf("%s\n", buffer);   
+        printf("REQUEST:\n%s\n", buffer);   
 
         struct ReturnObject* retObject = getFunction(graph, buffer);
         int httpStatus = retObject->httpStatus, node1 = retObject->firstNodeId, node2 = retObject->secondNodeId;
@@ -300,7 +301,7 @@ int main(int argc, char *argv[])
             //printf("\nboth nodes\n");
             snprintf(reply, 1024, "HTTP/1.1 200 OK\nContent-Length: %f\nContent-Type: application/json\n\n{\"node_a_id\":%i,\"node_b_id\":%i}\r\n", 27 + floor(log10(abs(node1)) + 1) + floor(log10(abs(node2)) + 1), node1, node2);
         }
-        printf("PRINTING JSON: %s\n", reply);
+        //printf("PRINTING JSON: %s\n", reply);
         write(new_socket, reply, strlen(reply));
         free(retObject);
         close(new_socket);    
